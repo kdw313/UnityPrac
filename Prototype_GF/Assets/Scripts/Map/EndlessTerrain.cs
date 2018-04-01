34166250600397c31fb0db0cd9f5d5f6c873e175
Shawn Kim <sdf1342@gmail.com>
2018-04-01 14:33:22 -0700

    Endless map

--- Prototype_GF/Assets/Scripts/Map/EndlessTerrain.cs
+++ Prototype_GF/Assets/Scripts/Map/EndlessTerrain.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class EndlessTerrain:MonoBehaviour {
 	
-	public const float maxViewDst = 300; 
+	public const float maxViewDst = 450; 
 	public Transform viewer; 
 
 	public static Vector2 viewerPosition; 
@@ -13,6 +13,7 @@ public class EndlessTerrain:MonoBehaviour {
 	int chunkVisibleInViewDst; 
 
 	Dictionary < Vector2, TerrainChunk > terrainChunkDictionary = new Dictionary < Vector2, TerrainChunk > (); 
+	List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
 
 
 	void Start() {
@@ -20,12 +21,21 @@ public class EndlessTerrain:MonoBehaviour {
 		chunkVisibleInViewDst = Mathf.RoundToInt (maxViewDst / chunkSize); 
 	}
 
+
 	void Update() {
 		viewerPosition = new Vector2(viewer.position.x, viewer.position.z); 
 		UpdateVisibleChunks(); 
 	}
 
+
 	void UpdateVisibleChunks() {
+
+		for (int i = 0; i < terrainChunksVisibleLastUpdate.Count; i++) {
+			terrainChunksVisibleLastUpdate [i].SetVisible (false);
+		}
+
+		terrainChunksVisibleLastUpdate.Clear ();
+
 		int currentChunkCoordX = Mathf.RoundToInt (viewerPosition.x / chunkSize); 
 		int currentChunkCoordY = Mathf.RoundToInt (viewerPosition.y / chunkSize); 
 
@@ -35,21 +45,29 @@ public class EndlessTerrain:MonoBehaviour {
 
 				if (terrainChunkDictionary.ContainsKey (viewChunkCoord)) {
 					terrainChunkDictionary[viewChunkCoord].UpdateTerrainChunk(); 
+
+					if (terrainChunkDictionary [viewChunkCoord].IsVisible()) {
+						terrainChunksVisibleLastUpdate.Add (terrainChunkDictionary [viewChunkCoord]);
+					}
+						
 				}
 				else {
-					terrainChunkDictionary.Add (viewChunkCoord, new TerrainChunk (viewChunkCoord, chunkSize)); 
+					terrainChunkDictionary.Add (viewChunkCoord, new TerrainChunk (viewChunkCoord, chunkSize, transform)); 
 				}
 
 			}
 		}
 	}
 
+
 	public class TerrainChunk {
+		
 		GameObject meshObject; 
 		Vector2 position; 
 		Bounds bounds; 
 
-		public TerrainChunk(Vector2 coord, int size) {
+
+		public TerrainChunk(Vector2 coord, int size, Transform parent) {
 			position = coord * size; 
 			bounds = new Bounds(position, Vector2.one * size); 
 			Vector3 positionV3 = new Vector3(position.x, 0, position.y); 
@@ -57,8 +75,10 @@ public class EndlessTerrain:MonoBehaviour {
 			meshObject = GameObject.CreatePrimitive(PrimitiveType.Plane); 
 			meshObject.transform.position = positionV3; 
 			meshObject.transform.localScale = Vector3.one * size /10f; 
+			meshObject.transform.parent = parent;
 			SetVisible(false); 
 		}
+
 
 		public void UpdateTerrainChunk() {
 			float viewrDstFromNearestEdge = Mathf.Sqrt(bounds.SqrDistance (viewerPosition)); 
@@ -66,8 +86,13 @@ public class EndlessTerrain:MonoBehaviour {
 			SetVisible(visible); 
 		}
 
+
 		public void SetVisible(bool visible) {
 			meshObject.SetActive(visible); 
+		}
+
+		public bool IsVisible() {
+			return meshObject.activeSelf; 
 		}
 
 	}
